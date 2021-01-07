d8e72716d744c1ece5aaeb80ea6193d6805e67c4
Damin <mm0133>
2021-01-07 22:06:19 +0900

    시험감독

---
+++ 1-4.py
@@ -0,0 +1,13 @@
+#백준 13458 시험감독/ 13분
+N=int(input())
+A=list(map(int, input().split()))
+B,C= map(int, input().split())
+su=N
+for i in range(N):
+    A[i]-=B+1
+    if A[i]>=0:
+        tem= (A[i])//C+1
+        su+=tem
+
+print(su)
+
